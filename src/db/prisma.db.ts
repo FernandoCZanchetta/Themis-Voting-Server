0c88effefd537423b9adbb9aabd2dc4f79efd438
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-23 19:29:56 -0300

    Lint code and add .env checks for prisma.db.ts

--- src/db/prisma.db.ts
+++ src/db/prisma.db.ts
@@ -4,7 +4,14 @@ import { PrismaClient } from '@generated'
 
 dotenv.config()
 
-const prismaPgAdapter = new PrismaPg({ connectionString: `${process.env.PRISMA_DATABASE_URL}` })
+const { PRISMA_DATABASE_URL } = process.env
+
+if (!process.env.PRISMA_DATABASE_URL) {
+  console.error('[ERROR] [Prisma DB] PRISMA_DATABASE_URL not defined in .env!')
+  throw new Error('[ERROR] [Prisma DB] PRISMA_DATABASE_URL not defined in .env!')
+}
+
+const prismaPgAdapter = new PrismaPg({ connectionString: PRISMA_DATABASE_URL })
 const prismaClient = new PrismaClient({ adapter: prismaPgAdapter })
 
 export default prismaClient
