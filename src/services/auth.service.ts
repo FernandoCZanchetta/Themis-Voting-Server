9cf15d2bbb5b13d5d10a73f713e097e688e6dbb7
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-24 13:20:08 -0300

    [Services] Create service to authenticate users

---
+++ src/services/auth.service.ts
@@ -0,0 +1,61 @@
+import jwt from 'jsonwebtoken'
+import { scrapeJupiterUser } from './scrapper.service'
+import { prismaClient } from '@db'
+
+const { JWT_SIGNING_SECRET } = process.env
+
+export async function authFromJupiterWeb(nUSP: string, uniquePassword: string) {
+  console.info('[Auth Service] Getting user data...')
+  const scrappedUser = await scrapeJupiterUser(nUSP, uniquePassword)
+
+  console.info('[Auth Service] Check course, institute and campus and upsert DB...')
+  await Promise.all([
+    scrappedUser.course && scrappedUser.courseCode ?
+      prismaClient.course.upsert({
+        where: { code: scrappedUser.courseCode },
+        update: { name: scrappedUser.course },
+        create: { code: scrappedUser.courseCode, name: scrappedUser.course },
+      }) :
+      Promise.resolve(),
+
+    scrappedUser.institute && scrappedUser.instituteCode ?
+      prismaClient.institute.upsert({
+        where: { code: scrappedUser.instituteCode },
+        update: { name: scrappedUser.institute },
+        create: { code: scrappedUser.instituteCode, name: scrappedUser.institute },
+      }) :
+      Promise.resolve(),
+
+    scrappedUser.campus ?
+      prismaClient.campus.upsert({
+        where: { name: scrappedUser.campus },
+        update: {},
+        create: { name: scrappedUser.campus },
+      }) :
+      Promise.resolve(),
+  ])
+
+  console.info('[Auth Service] Format user info...')
+  const userData = {
+    course: scrappedUser.course ?? null,
+    courseCode: scrappedUser.courseCode ?? null,
+    institute: scrappedUser.institute ?? null,
+    instituteCode: scrappedUser.instituteCode ?? null,
+    campus: scrappedUser.campus ?? null,
+  }
+
+  console.info('[Auth Service] Upsert user in DB...')
+  await prismaClient.user.upsert({
+    where: { nUSPHash: scrappedUser.nUSPHash },
+    update: userData,
+    create: {
+      nUSPHash: scrappedUser.nUSPHash,
+      ...userData,
+    },
+  })
+
+  console.info('[Auth Service] Generate JWT token...')
+  const jwtToken = jwt.sign(scrappedUser, JWT_SIGNING_SECRET!, { expiresIn: '30m' })
+
+  return { jwtToken }
+}
