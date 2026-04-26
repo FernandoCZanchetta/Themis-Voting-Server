d25207678e758973ba052fd8d51e7a2e0d426fc5
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-26 15:44:10 -0300

    [Utils] Update eligibility util to comply with prisma query filtering instead of memory filtering

--- src/utils/eligibility.util.ts
+++ src/utils/eligibility.util.ts
@@ -1,24 +1,22 @@
-import { Voting } from '@generated'
+import { VotingWhereInput } from '@generated'
 import { AuthUser } from '@types'
 
-export function isUserEligible(user: AuthUser, voting: Voting) {
-  if (voting.allowedCourses.length > 0) {
-    if (user.course && voting.allowedCourses.includes(user.course)) {
-      return true
-    }
-  }
-
-  if (voting.allowedInstitutes.length > 0) {
-    if (user.institute && voting.allowedInstitutes.includes(user.institute)) {
-      return true
-    }
-  }
-
-  if (voting.allowedCampi.length > 0) {
-    if (user.campus && voting.allowedCampi.includes(user.campus)) {
-      return true
-    }
-  }
-
-  return false
+export function buildVotingEligibilityWhereFilter(user: AuthUser): VotingWhereInput[] {
+  return [
+    {
+      OR: user.course
+        ? [{ allowedCourses: { isEmpty: true } }, { allowedCourses: { has: user.course } }]
+        : [{ allowedCourses: { isEmpty: true } }],
+    },
+    {
+      OR: user.institute
+        ? [{ allowedInstitutes: { isEmpty: true } }, { allowedInstitutes: { has: user.institute } }]
+        : [{ allowedInstitutes: { isEmpty: true } }],
+    },
+    {
+      OR: user.campus
+        ? [{ allowedCampi: { isEmpty: true } }, { allowedCampi: { has: user.campus } }]
+        : [{ allowedCampi: { isEmpty: true } }],
+    },
+  ]
 }
