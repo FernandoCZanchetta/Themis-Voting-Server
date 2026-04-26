0cbef4e58205dd34c626418f11d5fcfd3e9f8acf
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-26 14:41:36 -0300

    [Schemas] Update Voting Schema to comply with the VotingByID Route

--- src/schemas/voting.schema.ts
+++ src/schemas/voting.schema.ts
@@ -4,4 +4,9 @@ export const votingsQuerySchema = z.object({
   status: z.enum(['active', 'upcoming', 'finished']).optional(),
 })
 
+export const votingsParamsSchema = z.object({
+  id: z.uuid(),
+})
+
 export type VotingsQuerySchema = z.infer<typeof votingsQuerySchema>
+export type VotingsParamsSchema = z.infer<typeof votingsParamsSchema>
