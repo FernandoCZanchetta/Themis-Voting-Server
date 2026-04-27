d12ddc00f50f52937782a02f2214911c9ba46a92
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 07:00:26 -0300

    [Schemas] Create Audit Schema to check the request body with the vote receipt data

---
+++ src/schemas/audit.schema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+export const auditSchema = z.object({
+  hash: z.hash('sha512').transform(h => h.toLowerCase()),
+  nonce: z.string().regex(/^[a-f0-9]{32}$/i, 'Invalid nonce!').transform(n => n.toLowerCase()),
+  signature: z.hash('sha512').transform(s => s.toLowerCase()),
+})
+
+export type AuditRequestBody = z.infer<typeof auditSchema>
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
+export * from './audit.schema'
 export * from './auth.schema'
 export * from './vote.schema'
 export * from './voting.schema'
