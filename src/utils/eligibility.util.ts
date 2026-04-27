08322fa917e317235763f21df79640a2e0f54caa
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-04-27 06:08:57 -0300

    [Utils] Update eligibility util to allow memory filtering and prevent excessive DB queries

--- src/utils/eligibility.util.ts
+++ src/utils/eligibility.util.ts
@@ -1,4 +1,4 @@
-import { VotingWhereInput } from '@generated'
+import { Voting, VotingWhereInput } from '@generated'
 import { AuthUser } from '@types'
 
 export function buildVotingEligibilityWhereFilter(user: AuthUser): VotingWhereInput[] {
@@ -13,10 +13,26 @@ export function buildVotingEligibilityWhereFilter(user: AuthUser): VotingWhereIn
         ? [{ allowedInstitutes: { isEmpty: true } }, { allowedInstitutes: { has: user.institute } }]
         : [{ allowedInstitutes: { isEmpty: true } }],
     },
+    /**
     {
       OR: user.campus
         ? [{ allowedCampi: { isEmpty: true } }, { allowedCampi: { has: user.campus } }]
         : [{ allowedCampi: { isEmpty: true } }],
     },
+     */
   ]
+}
+
+export function isUserEligible(user: AuthUser, voting: Voting) {
+  if (voting.allowedCourses.length === 0 && voting.allowedInstitutes.length === 0 && voting.allowedCampi.length === 0) {
+    return true
+  }
+
+  const eligibleCourse = voting.allowedCourses.length === 0 || !!user.course && voting.allowedCourses.includes(user.course)
+
+  const eligibleInstitute = voting.allowedInstitutes.length === 0 || !!user.institute && voting.allowedInstitutes.includes(user.institute)
+
+  // const eligibleCampus = voting.allowedCampi.length === 0 || !!user.campus && voting.allowedCampi.includes(user.campus)
+
+  return eligibleCourse && eligibleInstitute
 }
